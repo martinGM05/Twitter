55c1db435324bbc90932b8f12dfeac2fc0683391
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-04-21 14:29:57 -0500

    Creating the UserService service

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -1,8 +1,8 @@
-const UseService = require('./../../app/services/UserService')
+const UserService = require('./../../app/services/UserService')
 
 describe("Tests for UserServices", () => {
     test("1. Create a new user using the UserServices", () => {
-        const user = UserServices.create(1, "martingm", "Martin")
+        const user = UserService.create(1, "martingm", "Martin")
         expect(user.username).toBe("martingm")
         expect(user.name).toBe("Martin")
         expect(user.id).toBe(1)
