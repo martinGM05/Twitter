369c8d88ae2d9ec6a8d8b5a824377c35747053d2
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-04-21 12:45:42 -0500

    Agregue la prueba y ejecute con resultado de fallido

---
+++ test/models/User.test.js
@@ -0,0 +1,15 @@
+const User = require('../../app/models/User')
+
+describe("Unit testing for User Class", () => {
+    test('1) Create an User object' , () => {
+        const user = new User(1, 'martinGM05', 'Martin', 'Bio', 'dateCreated', 'lastUpdated')
+
+        expect(user.id).toBe(1)
+        expect(user.username).toBe('martinGM05')
+        expect(user.name).toBe('Martin')
+        expect(user.bio).toBe('Bio')
+        expect(user.dateCreated).toBe('dateCreated')
+        expect(user.lastUpdated).toBe('lastUpdated')
+
+    })
+})
