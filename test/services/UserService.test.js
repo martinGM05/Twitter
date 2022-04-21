f3a9a2f7b3c7a1e7f04012fe288799ab24e2df3e
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-04-21 14:38:09 -0500

    Create a second test for call to GetInfo

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -1,11 +1,19 @@
 const UserService = require('./../../app/services/UserService')
 
 describe("Tests for UserServices", () => {
-    test("1. Create a new user using the UserServices", () => {
+    test("1) Create a new user using the UserServices", () => {
         const user = UserService.create(1, "martingm", "Martin")
         expect(user.username).toBe("martingm")
         expect(user.name).toBe("Martin")
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
+    })
+
+    test("2) Get all user data in a list", () => {
+        const user = UserService.create(1, "martingm", "Martin")
+        const userInfoList = UserService.getInfo(user)
+        expect(userInfoList[0]).toBe('martingm')
+        expect(userInfoList[1]).toBe('Martin')
+        expect(userInfoList[2]).toBe('Son Bio')
     })
 })
