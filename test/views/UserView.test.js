0be26b2a738b4acd8c6b35b316a4bd53240d5862
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-04-21 20:34:31 -0500

    The previous solution also resolves the conflict if there is a missing property in the payload

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -11,4 +11,9 @@ describe("Tests for UserView", () => {
         const result = UserView.createUser(payload)
         expect(result.error).toMatch(/necesitan tener un valor válido/)
     })
+    test("3) Return an error object when try to create a new user with a payload with missing properties", () => {
+        const payload = {username: "Username"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor válido/)
+    })
 })
