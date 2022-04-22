cd8802511797f26e6fb3b0dad214dbd309792bc6
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-04-21 20:23:00 -0500

    Solution for the second test

--- app/views/userView.js
+++ app/views/userView.js
@@ -3,7 +3,13 @@ const useService = require('./../../app/services/UserService')
 class UserView {
     static createUser(payload) {
         if (payload) {
-            return useService.create(payload)
+            if (payload.username && payload.name && payload.id) {
+                return useService.create(payload)
+            }else{
+                return {
+                    error: "necesitan tener un valor válido"
+                }
+            }
         } else {
             return {
                 error: 'payload no existe'
