88bda5cc6200edff11fec92d9cc34d27bd7ff116
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-04-21 20:21:08 -0500

    Create the test for Return an error object when try to create a new user with a payload with  invalid propierties

--- app/views/userView.js
+++ app/views/userView.js
@@ -3,10 +3,7 @@ const useService = require('./../../app/services/UserService')
 class UserView {
     static createUser(payload) {
         if (payload) {
-            const user = useService.create(payload.id, payload.username, payload.name)
-            return {
-                user: user,
-            }
+            return useService.create(payload)
         } else {
             return {
                 error: 'payload no existe'
--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -1,9 +1,14 @@
 const UserView = require('./../../app/views/userView')
 
 describe("Tests for UserView", () => {
-    test("Return an error object when try to create a new user with an null payload", () => {
+    test("1) Return an error object when try to create a new user with an null payload", () => {
         const payload = null
         const result = UserView.createUser(payload)
         expect(result.error).toMatch(/payload no existe/)
+    })
+    test("2) Return an error object when try to create a new user with a payload with invalid properties", () => {
+        const payload = {username: null, name: 21, id: "id" }
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor válido/)
     })
 })
